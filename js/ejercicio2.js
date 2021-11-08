cf2c1b5bc1b6b38ec414867aa316baaa41b35416
MichaelD002 <89713959+MichaelD002@users.noreply.github.com>
2021-11-08 15:23:31 -0500

    Add files via upload

---
+++ js/ejercicio2.js
@@ -0,0 +1,18 @@
+/*Escribe un programa que piense un número de forma aleatoria
+del 1 al 10 y le pida al usuario que lo trate de adivinar.
+Si el número es correcto debe imprimir en la consola
+"Felicitaciones, ese era!", de lo contrario debe imprimir
+"Lo siento, intenta nuevamente!" */
+let num = parseInt(prompt("intente adivinar el nuemro"))
+function rand(min, max){
+    return Math.floor(Math.random() * (max - min) + min)
+}
+console.log(rand(1,10)) 
+
+let resu = rand(1,10)
+
+if(resu == num){
+    console.log("Felicitaciones, ese era!")
+}else{
+    console.log("Lo siento, intenta nuevamente!")
+}
---
+++ js/ejercicio4.js
@@ -0,0 +1,13 @@
+/*Escribe un programa que le pida un número al usuario e imprima en la
+consola si el número si es mayor, menor o igual que 10.
+Si el número es menor a 10 debe imprimir "El número es menor que 10".
+Si el número es mayor a 10 debe imprimir "El número es mayor que 10".
+Si el número es igual a 10 debe imprimir "El número es igual a 10".*/
+let num = parseInt(prompt("Ingrese un numero"))
+if(num<10){
+    console.log(`El número ${num} es menor que 10`)
+}else if(num>10){
+    console.log(`El número ${num} es mayor que 10`)
+}else{
+    console.log(`El número ${num} es igual a 10`)
+}
